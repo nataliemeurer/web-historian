e2ca17d6e61b33889f93acc7216e1fce32fbadcc
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-10-02 10:22:24 -0700

    added http.

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var http = require('http');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -54,6 +55,6 @@ exports.isURLArchived = function(url){
   return fs.existsSync(exports.paths.archivedSites + '/' + url);
 };
 
-exports.downloadUrls = function(){
+exports.downloadUrls = function(url){
 
 };
